52b35520b818cf926ccea597823f71efb7b4efaf
stephanyalcaide <81532183+stephanyalcaide@users.noreply.github.com>
2021-11-07 20:15:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -157,5 +157,5 @@ function writePassword() {
   passwordText.value = password1;
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 }
+generateBtn.addEventListener("click", writePassword);
